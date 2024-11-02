ad0d7bb7811429b735b1a09ca4b9f1d20d6b529e
Luca Stefani <luca.stefani.ge1@gmail.com>
2024-11-02 11:45:21 +0100

    Add vendor/bliss to kotlin plugins neverallow
    We don't want to fork prebuilts/sdk just for that
    
    Change-Id: I5f6ff87e341d2391354b1379291de92318599eeb
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- android/neverallow.go
+++ android/neverallow.go
@@ -290,6 +290,7 @@ func createKotlinPluginRule() []Rule {
 		// Actual path prebuilts/sdk/current/androidx/m2repository/androidx/compose/compiler/compiler-hosted
 		"prebuilts/sdk/current/androidx",
 		"external/kotlinc",
+		"vendor/bliss/kotlin",
 	}
 
 	return []Rule{
