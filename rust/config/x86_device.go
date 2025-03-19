c926903c590c5e9cde81399efbb843889edcf910
hmtheboy154 <buingoc67@gmail.com>
2025-03-19 15:43:52 -0400

    Rust: Use nehalem cpu-target when building with sandybridge arch variant
    On Android, sandybridge doesn't include AVX support, so it's more like
    nehalem target on Rust toolchain or GCC.
    
    Change-Id: I93d1a7c63fe309fc5d3e58c93c3248c937b5aa48
    Signed-off-by: hmtheboy154 <buingoc67@gmail.com>

--- rust/config/x86_device.go
+++ rust/config/x86_device.go
@@ -31,7 +31,7 @@ var (
 		"broadwell":   []string{"-C target-cpu=broadwell"},
 		"haswell":     []string{"-C target-cpu=haswell"},
 		"ivybridge":   []string{"-C target-cpu=ivybridge"},
-		"sandybridge": []string{"-C target-cpu=sandybridge"},
+		"sandybridge": []string{"-C target-cpu=nehalem"},
 		"silvermont":  []string{"-C target-cpu=silvermont"},
 		"skylake":     []string{"-C target-cpu=skylake"},
 		//TODO: Add target-cpu=stoneyridge when rustc supports it.
